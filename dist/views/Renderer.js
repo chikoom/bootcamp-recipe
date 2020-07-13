bcefce1f59c639c8d2b231a1d2b36a8d6286f4b8
Chikoom <callforcopy@gmail.com>
2020-07-13 12:57:46 +0300

    Some Style added

--- dist/views/Renderer.js
+++ dist/views/Renderer.js
@@ -1,8 +1,8 @@
 const template = Handlebars.compile($(`#search-results-template`).html())
 export class Renderer{
   renderResults(data){
+    $('#input-search').val('')
     const serchHTML = template({ data })
     $('#results').empty().append(serchHTML)
-    console.log(data)
   }
 }
