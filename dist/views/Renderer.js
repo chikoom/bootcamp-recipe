047c156b8cddc3b61b2f8bf5fc01f97cd30c8389
Chikoom <callforcopy@gmail.com>
2020-07-13 12:24:41 +0300

    Renderer and Handlebars working

--- dist/views/Renderer.js
+++ dist/views/Renderer.js
@@ -1,5 +1,8 @@
+const template = Handlebars.compile($(`#search-results-template`).html())
 export class Renderer{
   renderResults(data){
+    const serchHTML = template({ data })
+    $('#results').empty().append(serchHTML)
     console.log(data)
   }
 }
