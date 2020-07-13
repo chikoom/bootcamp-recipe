c96d17787818f76ad403b24040ed7ee20a26f791
Chikoom <callforcopy@gmail.com>
2020-07-13 11:48:42 +0300

    API call succeeded

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,9 +2,21 @@ const express = require('express')
 const urllib = require('urllib')
 
 const router = express.Router()
+const APIURL = 'https://recipes-goodness.herokuapp.com/recipes/'
 
 router.get('/sanity', (req, res) => {
   res.send('OK')
+})
+
+router.get('/recipes/:ingredient', (req, res) => {
+  urllib.request(`${APIURL}${req.params.ingredient}`,
+    function (err, data) {
+    if (err) {
+      console.log(`Error recieving recieps`)
+      res.send(`Error recieving recieps`)
+    }
+    res.send(data)
+  })
 })
 
 module.exports = router
